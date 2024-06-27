98f7e33e59feccb20a232f06005787aa0fb2989e
fumiko <2394425147@qq.com>
2024-06-27 18:17:35 +0800

    bugfix: Store initial pointer position and send stored code when releasing pointer
    This fixes a case when a finger slides from a key to another, causing code 1 down and code 2 up to be sent, which doesn't make key 1 to be released when the finger is

--- app/src/main/java/gay/nihil/lena/midikeypad/ManiaStaticKeypadHandler.java
+++ app/src/main/java/gay/nihil/lena/midikeypad/ManiaStaticKeypadHandler.java
@@ -3,23 +3,39 @@ package gay.nihil.lena.midikeypad;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 public class ManiaStaticKeypadHandler extends KeypadHandler {
     public ManiaStaticKeypadHandler(View view) {
         super(view);
     }
 
+    // stores pairs of: action index -> code
+    // this allows pointer up events to release a pointer's original key,
+    // preventing a situation where the finger slides to a different key and a wrong code would be fired
+    private static final HashMap<Integer, Integer> motions = new HashMap<>();
+
     @Override
     public KeypadData HandleMotionEvent(MotionEvent event) {
-        float x = event.getX(event.getActionIndex());
-        float width = view.getWidth();
-        float keyWidth = width / 4;
+        // https://android-developers.googleblog.com/2010/06/making-sense-of-multitouch.html
+        final int pointerIndex = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+        final int pointerId = event.getPointerId(pointerIndex);
+        int actionType = event.getActionMasked();
 
-        int code = 60 + (int) (x / keyWidth);
+        if (actionType == MotionEvent.ACTION_DOWN || actionType == MotionEvent.ACTION_POINTER_DOWN) {
+            float x = event.getX(pointerIndex);
+            float width = view.getWidth();
+            float keyWidth = width / 4;
 
-        int a = event.getActionMasked();
-        if (a == MotionEvent.ACTION_DOWN || a == MotionEvent.ACTION_POINTER_DOWN) {
+            int code = 60 + (int) (x / keyWidth);
+            motions.put(pointerId, code);
+
             return new KeypadData(true, true, code);
-        } else if (a == MotionEvent.ACTION_UP || a == MotionEvent.ACTION_POINTER_UP) {
+        } else if (actionType == MotionEvent.ACTION_UP || actionType == MotionEvent.ACTION_POINTER_UP) {
+            Integer code = Objects.requireNonNull(motions.get(pointerId));
+            motions.remove(pointerId);
             return new KeypadData(true, false, code);
         } else {
             return new KeypadData(false, false, 0);
